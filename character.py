2866381e4f55ceecff46fe67e904a8c4bf1fbe89
kylelau519 <kylelau519@gmail.com>
2019-02-04 03:33:35 +0800

    improved gravity

--- character.py
+++ character.py
@@ -6,39 +6,18 @@ class character(object):
         self.y = y
         self.dx = 0
         self.dy = 0
-
         self.width = 10
         self.height = 20
         self.vel = 4
         self.jumping = False
-        self.jumpspeed = 5
-        self.jc = 16
-        self.jumpCount = 16
     def hitbox(self):
         return pg.Rect(self.x, self.y, self.width, self.height)
     def accelerate():
         ''
-    def jump(self,colli,map):
-        if self.jumpCount >= -self.jc:
-            if self.jumpCount > 0:
-                self.y -= self.jumpspeed
-                if colli(map):
-                    self.y += self.jumpspeed
-                    self.jumping = False
-                    self.jumpCount = self.jc
-            else:
-                self.y += self.jumpspeed
-                if colli(map):
-                    self.y -= self.jumpspeed
-                    self.jumping = False
-                    self.jumpCount = self.jc
-            self.jumpCount -= 1
-        else:
-            self.jumping = False
-            self.jumpCount = self.jc
 
 
-
+    def jump(self):
+        self.dy = -22
 
     def draw(self,screen):
         pg.draw.rect(screen,(255,255,255),pg.Rect(50, self.y, self.width, self.height))
--- mario.py
+++ mario.py
@@ -19,14 +19,15 @@ def isEarth(map):
             return True
     return False
 
-def gravity():
-    if not(isEarth(map) or mario.jumping):
-        mario.y += 5
+def vert_move():
+    mario.y += mario.dy
+    while colli(map):
+        mario.jumping = False
+        mario.y -= 1
 
-def new_gravity():
-    if not(isEarth(map) or mario.jumping):
-        mario.dy += 1
-        mario.y += mario.dy
+def gravity():
+    if not isEarth(map):
+        mario.dy += 3
 
 map = map1_1()
 
@@ -34,14 +35,11 @@ pg.init()
 screen = pg.display.set_mode((500,300))
 pg.display.set_caption('Mario')
 clock = pg.time.Clock()
-moveCam = False
-
 mario = character(50,260)
 
 run = True
 while run:
     clock.tick(30)
-    print mario.x,mario.y
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -50,30 +48,28 @@ while run:
             if event.key == pg.K_ESCAPE:
                 run = False
 
-
     screen.fill((0, 0, 0))
     draw(map,mario,screen)
-    gravity()
     mario.draw(screen)
-
+    vert_move()
+    gravity()
 
     keys = pg.key.get_pressed()
     if keys[pg.K_LEFT] and mario.x > 0:
         mario.x -= mario.vel
-
         if colli(map):
             mario.x += mario.vel
+
     if keys[pg.K_RIGHT] and mario.x < 9750:
         mario.x += mario.vel
-
         if colli(map):
             mario.x -= mario.vel
 
-    if not(mario.jumping):
-        if keys[pg.K_UP] and isEarth(map):
-            mario.jumping = True
-    else:
-        mario.jump(colli,map)
+    if isEarth(map):
+        if keys[pg.K_UP]:
+            if not(mario.jumping):
+                mario.jumping = True
+                mario.jump()
 
     pg.display.update()
 pg.quit()
