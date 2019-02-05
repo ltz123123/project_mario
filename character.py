d5c11af70cf8995abfec8654503cdf0a10c64ee8
ltz123123 <u3537457@hku.hk>
2019-02-06 01:28:02 +0800

    c

--- character.py
+++ character.py
@@ -2,28 +2,63 @@ import pygame as pg
 
 
 class character(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, img):
         self.x = x
         self.y = y
-        self.dx = 0
+        self.left = False
+        self.right = False
+        self.walk_tick = 0
         self.dy = 0
         self.width = 10
         self.height = 20
         self.vel = 5
         self.jumping = False
-
+        self.img = img
 
     def jump(self):
-        self.dy = -17
+        self.dy = -13
 
     def hitbox(self):
         return pg.Rect(self.x, self.y, self.width, self.height)
 
     def head(self):
-        return pg.Rect(self.x, self.y, self.width, self.height * 0.5)
+        return pg.Rect(self.x, self.y, self.width, self.height * 0.2)
 
-    def accelerate():
-        ''
+    def feet(self):
+        return pg.Rect(self.x, self.y + self.height * 0.8, self.width, self.height * 0.2)
 
-    def draw(self, screen):
-        pg.draw.rect(screen, (255, 255, 255), self.hitbox())
+    def draw(self, window):
+        if self.walk_tick + 1 > 40:
+            self.walk_tick = 0
+        if not self.jumping:
+            if self.left:
+                window.blit(self.img[self.walk_tick // 10], (self.x, self.y))
+                self.walk_tick += 1
+            elif self.right:
+                window.blit(self.img[self.walk_tick // 10], (self.x, self.y))
+                self.walk_tick += 1
+            else:
+                window.blit(self.img[self.walk_tick // 10], (self.x, self.y))
+        else:
+            window.blit(self.img[4], (self.x, self.y))
+            self.walk_tick += 1
+
+
+class enemy(character):
+    def __init__(self, x, y, img):
+        character.__init__(self, x, y, img)
+        self.width = 15
+        self.height = 15
+        self.vel = 2
+        self.right = True
+        self.dy = 0
+
+    def run(self):
+        if self.left:
+            self.x -= self.vel
+        elif self.right:
+            self.x += self.vel
+
+    def draw(self, window):
+        pg.draw.rect(window,(255,255,255),(self.x,self.y,self.width,self.height),3)
+
--- mario.py
+++ mario.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from character import character
+from character import *
 from earth import *
 
 
@@ -10,75 +10,114 @@ def draw(map, map_q, mario, screen):
         j.draw(mario, screen)
 
 
-
-def colli(map, map_q):
+def colli(who, map, map_q):
     for i in map:
-        if mario.hitbox().colliderect(i.block()):
+        if who.hitbox().colliderect(i.block()):
             return True
     for j in map_q:
-        if mario.hitbox().colliderect(j.block()):
+        if who.hitbox().colliderect(j.block()):
             return True
     return False
 
 
-def isEarth(map, map_q):
+def isEarth(who, map, map_q):
     for i in map:
-        if mario.hitbox().colliderect(i.earth()):
+        if who.hitbox().colliderect(i.earth()):
             return True
     for j in map_q:
-        if mario.hitbox().colliderect(j.earth()):
+        if who.hitbox().colliderect(j.earth()):
             return True
     return False
 
 
 def hit_ceiling_hitbox():
-    for i in map_qmark:
+    for i in map_breakable:
         if mario.head().colliderect(i.ceiling_hitbox()):
             i.broken = True
             return True
+
+
 def hit_ceiling():
-    for i in map_qmark:
+    for i in map_breakable:
         if mario.hitbox().colliderect(i.ceiling()):
             return True
     return False
 
-def vert_move():
-    mario.y += mario.dy
+
+def gravity(who):
+    who.y += who.dy
     if hit_ceiling():
-        mario.dy = 5
+        who.dy = 3
     else:
-        while colli(map, map_qmark):
-            mario.jumping = False
-            mario.y -= 1
-
-
-def gravity():
-    if not isEarth(map, map_qmark):
-        mario.dy += 2
+        while colli(who, map, map_breakable):
+            who.jumping = False
+            who.y -= 1
+    if not isEarth(who, map, map_breakable):
+        if who.dy < 15:
+            who.dy += 1
     else:
-        while colli(map, map_qmark):
-            mario.jumping = False
-            mario.y -= 1
+        while colli(who, map, map_breakable):
+            who.jumping = False
+            who.dy = 0
+            who.y -= 1
 
 
 def camera(sign):
     global moveCam
     if mario.x > 200:
         moveCam = True
+    elif map[0].x > -5 or map[3].x < -1985:
+        moveCam = False
     if moveCam:
         for i in map:
             i.x += mario.vel * sign
-        for j in map_qmark:
+        for j in map_breakable:
             j.x += mario.vel * sign
         mario.x += mario.vel * sign
+        m1.x += mario.vel * sign
+
+
+def auto_run():
+    def touch_pipe():
+        for i in [p1, p2, p3, p4, p5, p6, s11]:
+            if m1.hitbox().colliderect(i.block()):
+                return True
+
+    if touch_pipe():
+        m1.y += 1
+        if m1.left:
+            m1.x += m1.vel
+            m1.left = False
+            m1.right = True
+        elif m1.right:
+            m1.x -= m1.vel
+            m1.right = False
+            m1.left = True
+    if m1.left:
+        m1.x -= m1.vel
+    elif m1.right:
+        m1.x += m1.vel
+    if not isEarth(m1, map, map_breakable):
+        if m1.dy < 15:
+            m1.dy += 1
+        m1.y += m1.dy
+        m1.jumping = True
+    else:
+        m1.dy = 0
+        m1.jumping = False
 
 
 pg.init()
 screen = pg.display.set_mode((500, 300))
 pg.display.set_caption('Mario')
 clock = pg.time.Clock()
-mario = character(50, 260)
+marioImg = [pg.image.load('1.png'), pg.image.load('2.png'), pg.image.load('3.png'), pg.image.load('4.png'),
+            pg.image.load('man.png')]
+mario = character(50, 260, marioImg)
 moveCam = False
+
+# test auto_run
+m1 = enemy(350, 135, None)
 
 run = True
 while run:
@@ -92,29 +131,35 @@ while run:
                 run = False
 
     screen.fill((0, 0, 0))
-    draw(map, map_qmark, mario, screen)
+    draw(map, map_breakable, mario, screen)
     mario.draw(screen)
-    vert_move()
-    gravity()
+    m1.draw(screen)
+    auto_run()
+    gravity(mario)
     hit_ceiling_hitbox()
-
 
     keys = pg.key.get_pressed()
     if keys[pg.K_LEFT] and mario.x > 0:
         mario.x -= mario.vel
         camera(1)
-        if colli(map, map_qmark):
+        mario.left = True
+        mario.right = False
+        if colli(mario, map, map_breakable):
             camera(-1)
             mario.x += mario.vel
-
-    if keys[pg.K_RIGHT]:
+    elif keys[pg.K_RIGHT]:
         mario.x += mario.vel
         camera(-1)
-        if colli(map, map_qmark):
+        mario.right = True
+        mario.left = False
+        if colli(mario, map, map_breakable):
             camera(1)
             mario.x -= mario.vel
+    else:
+        mario.left = False
+        mario.right = False
 
-    if isEarth(map, map_qmark):
+    if isEarth(mario, map, map_breakable):
         if keys[pg.K_UP]:
             if not mario.jumping:
                 mario.jumping = True
